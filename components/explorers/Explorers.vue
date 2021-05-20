a88823e422f3adfa29aa849cc106bcff80140ab4
Carlos Aparicio <carlos_non86@msn.com>
2021-05-20 08:00:48 +0200

    set phosphor icons

--- components/explorers/Explorers.vue
+++ components/explorers/Explorers.vue
@@ -1,12 +1,32 @@
 <template>
-    <div>
-        explorers
+  <div class="section_explorers min-h-screen grid grid-cols-1 grid-rows-1">
+    <div class="explorers col-span-full row-span-full">
+      <div
+        class="explorer"
+        v-for="(explorer, i) in content.data"
+        :key="i"
+      ><ph-arrow-left /></div>
     </div>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+//@ts-ignore
+import { PhArrowLeft } from 'phosphor-vue'
+
 export default Vue.extend({
-    name: "Explorers"
+  name: 'Explorers',
+  components: {
+    PhArrowLeft
+  },
+  data() {
+    return {
+      content: {},
+    }
+  },
+  async fetch() {
+    this.content = await (this as any).$content('03_sectionExplorers').fetch()
+  },
 })
 </script>
