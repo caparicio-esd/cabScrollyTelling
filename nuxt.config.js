9945882a9aed030ddf8c42618b9cdc0e590e15ae
Carlos Aparicio <carlos_non86@msn.com>
2021-05-20 09:44:22 +0200

    improve accesibility and seo meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,8 +10,8 @@ export default {
     },
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no' },
-      { hid: 'description', name: 'description', content: '' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Cab-Inta participation in Rover mars missions. Narrative interactive' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/img/favicon.ico' }],
   },
