be514ff9108d70ebcf54f91de927efef3d9c4ec4
caparicio-esd <caparicio@esdmadrid.es>
2021-06-03 15:47:31 +0200

    add splashscreen

---
+++ components/SplashScreen.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="splash"></div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'Splash',
+})
+</script>
+
+<style lang="postcss" scoped>
+.splash {
+}
+</style>
