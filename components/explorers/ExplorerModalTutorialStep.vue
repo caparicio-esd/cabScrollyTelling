bb10a1722b013aae9ed0a71ab93b3fb4528c1bbe
caparicio-esd <caparicio@esdmadrid.es>
2021-10-03 22:18:17 +0200

    modal tutorial placing

--- components/explorers/ExplorerModalTutorialStep.vue
+++ components/explorers/ExplorerModalTutorialStep.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="explorer_modal_tutorial_step" v-if="current">
+  <div class="explorer_modal_tutorial_step" v-if="current" :style="style" ref="step">
     <div class="explorer_modal_tutorial_step_holder">
       <div class="explorer_modal_tutorial_step_body">
         <div class="step">{{ description.step }}</div>
         <div class="content">{{ description.content }}</div>
       </div>
       <div class="explorer_modal_tutorial_step_cta">
-        <button class="nobg" @click="$emit('setCurrentStep', 'none')">No mostrar</button>
+        <button class="nobg" @click="$emit('setCurrentStep', 'none')">
+          No mostrar
+        </button>
         <button @click="$emit('setCurrentStep', 'next')">
           <span>OK</span>
           <ph-arrow-right :size="20" />
@@ -24,8 +26,43 @@ import { PhArrowRight } from 'phosphor-vue'
 export default Vue.extend({
   name: 'ExplorerModalTutorialStep',
   props: ['description', 'component', 'current'],
+  data() {
+    return {
+      style: {
+        top: "0", 
+        left: "0", 
+        position: "static", 
+        transform: "", 
+      } as CSSStyleDeclaration,
+    }
+  },
   components: {
     PhArrowRight,
+  },
+  methods: {
+    placeElement() {
+      const left = 0 + 
+        ((this.component.direction[1] == "left") ? -1 : 1 ) * 16 +
+        this.component.dom?.offsetLeft + 
+        ((this.component.direction[1] == "left") ? (this.$refs.step as HTMLElement)?.offsetWidth * -1 || 0 : 0) +
+        ((this.component.origin[1] == "left") ? 0 : this.component.dom?.offsetWidth) 
+
+      const top = 0 + 
+        ((this.component.direction[0] == "top") ? -1 : 1 ) * 16 +
+        this.component.dom?.offsetTop + 
+        ((this.component.direction[0] == "top") ? (this.$refs.step as HTMLElement)?.offsetHeight * -1 || 0 : 0) +
+        ((this.component.origin[0] == "top") ? 0 : this.component.dom?.offsetHeight) 
+      
+      this.style.position = 'absolute'     
+      this.style.transform = `translate(${left}px, ${top}px)`
+    },
+  },
+  async mounted() {
+    await this.$nextTick()
+    this.placeElement()
+  },
+  updated() {
+    this.placeElement()
   },
 })
 </script>
