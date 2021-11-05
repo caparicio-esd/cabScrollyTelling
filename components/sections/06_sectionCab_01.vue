c73595a0164d6e4c41cdb044fb187edabb4958d7
Lourdes <lourdesmarco.design@gmail.com>
2021-11-05 19:05:26 +0100

    Add cab_01 page final text

--- components/sections/06_sectionCab_01.vue
+++ components/sections/06_sectionCab_01.vue
@@ -7,23 +7,23 @@
     <div class="section_content_holder bg-transparent">
       <div class="grid_holder">
         <div class="col_01">
-          <div class="pic">pic</div>
+          <div class="pic"><img :src="content.assets.pictures[1]" alt="dibujo de las instalaciones del CAB"></div>
           <h2>
             {{ content.data.title}}
           </h2>
           <p v-for="(p, i) in content.data.mainText" :key="i">{{ p }}</p>
         </div>
         <div class="col_02">
-          <div class="pic">pic</div>
-          <div class="pic">pic</div>
-          <div class="pic">pic</div>
+          <div class="pic"><img :src="content.assets.pictures[0]" alt="dibujo de las instalaciones del CAB"></div>
+          <div class="pic"><img :src="content.assets.pictures[2]" alt="dibujo de las instalaciones del CAB"></div>
+          <div class="pic"><img :src="content.assets.pictures[3]" alt="dibujo de las instalaciones del CAB"></div>
         </div>
         <div class="col_03">
           <h2>
             {{ content.data.secondaryTitle}}
           </h2>
           <p v-for="(p, i) in content.data.secondaryText" :key="i">{{ p }}</p>
-          <div class="pic">pic</div>
+          <div class="pic"><img :src="content.assets.pictures[4]" alt="dibujo de las instalaciones del CAB"></div>
         </div>
       </div>
     </div>
@@ -53,7 +53,7 @@ export default Vue.extend({
   },
   mixins: [Scrollable],
   async fetch() {
-    this.content = await getContent(this, '06_SectionCab01')    
+    this.content = await getContent(this, '06_SectionCab01')
   },
   async mounted() {
     await this.$nextTick()
@@ -86,7 +86,10 @@ export default Vue.extend({
 
   .pic {
     @apply bg-gray-400 h-36 my-4;
+    overflow: hidden;
     img {
+      object-fit: cover;
+
     }
   }
 }
