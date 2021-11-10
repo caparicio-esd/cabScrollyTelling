bbdbe42f4f04980795c6e671425e7ad384b1467e
caparicio-esd <caparicio@esdmadrid.es>
2021-11-10 13:50:42 +0100

    smooth scrolling

--- components/ButtonNextScreen.vue
+++ components/ButtonNextScreen.vue
@@ -1,20 +1,37 @@
 <template>
-  <button
-    @click="$emit('goNextSlide')"
-    class="btn_primary btn_next">
-          <span>CONTINUAR</span>
-          <ph-arrow-down :size="20" />
-        </button>
+  <button class="btn_primary btn_next" @click="goToNextSlide">
+    <span>CONTINUAR</span>
+    <ph-arrow-down :size="20" />
+  </button>
 </template>
 
 <script>
 import Vue from 'vue'
 //@ts-ignore
 import { PhArrowDown } from 'phosphor-vue'
+import { mapGetters, mapState } from 'vuex'
+
 export default {
   components: {
-    PhArrowDown
+    PhArrowDown,
+  },
+  computed: {
+    ...mapGetters({
+      inWhichSceneIAm: 'main/inWhichSceneIAm',
+      scrollOffsetLimitsByScene: 'main/scrollOffsetLimitsByScene',
+      getSceneScrollOffset: 'main/getSceneScrollOffset',
+    }),
+    ...mapState({
+      height: (state) => state.main.ui.viewPort.height
+    })
+  },
+  methods: {
+    goToNextSlide() {
+      const currScene = this.inWhichSceneIAm
+      const sLimits = this.scrollOffsetLimitsByScene(currScene + 1)
+      const sDuration = this.getSceneScrollOffset(currScene + 1)
+      scrollTo(0, sLimits[0] + this.height * 2)
+    },
   },
 }
 </script>
-
