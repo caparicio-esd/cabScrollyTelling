9ca01c60a764665bb590bf1c73df89dbdf972d2f
Lourdes <lourdesmarco.design@gmail.com>
2021-11-14 20:18:10 +0100

    Add final images & texts to machine modals. Create Tab1 & Tab2 components

--- components/machines/MachineModal.vue
+++ components/machines/MachineModal.vue
@@ -1,136 +1,32 @@
 <template>
   <div class="machine_modal" :class="open ? 'opened' : ''">
     <div class="machine_modal_image">
-      <img :src="tab1.picture" :alt="`Imagen del ${machineData.machine}`">
-      <button class="cabBtn">
-      {{ machineData.cabInstrument }}
+      <img :src="tab1Active ? tab1.picture : tab2.picture" :alt="`Imagen del ${machineData.machine}`">
+      <div class="machine_title">{{ machineData.machine }}</div>
+      <button
+        class="cabBtn"
+        :class="tab2Active ? 'active' : ''"
+        @click="toggleTab"
+      >
+        {{ machineData.cabInstrument }}
       </button>
     </div>
 
-    <!-- TAB 1 - TODO: create new component-->
-    <div class="explorer_modal_content" v-if="tab1">
-      <!-- header -->
-      <div class="explorer_modal_content_header">
-        <div class="explorer_modal_content_agency_meta">
-          <div class="explorer_modal_content_land">
-            <div class="explorer_modal_content_land_picture">
-              <img :src="tab1.picture_flag" :alt="`bandera de ${tab1.meta.land}`">
-            </div>
-            <div class="explorer_modal_content_land_name">
-              {{ tab1.meta.land }}
-            </div>
-          </div>
-          <div class="explorer_modal_content_agency">
-            {{ tab1.meta.agency }}
-          </div>
-        </div>
-      </div>
-      <!-- title -->
-      <div class="explorer_modal_content_title">
-        <div class="explorer_modal_content_title_top">
-          <h3 class="title">{{ machineData.title }}</h3>
-          <div class="explorer_modal_content_success_flag">
-            {{ tab1.meta.status }}
-          </div>
-        </div>
-        <div class="explorer_modal_content_title_tagline">
-          {{ machineData.subtitle }}
-        </div>
-      </div>
-      <!-- meta -->
-      <div class="explorer_modal_content_meta">
-        <!-- launch / arrival-->
-        <div class="explorer_modal_content_meta_launch_arrival">
-          <div
-            class="explorer_modal_content_meta_launch"
-            v-if="tab1.meta.launch"
-          >
-            <span class="label">Lanzamiento</span>
-            <ph-dots-three :size="20" />
-            <span class="content">{{ tab1.meta.launch }}</span>
-          </div>
-          <div
-            class="explorer_modal_content_meta_arrival"
-            v-if="tab1.meta.arrival"
-          >
-            <span class="label">Llegada</span>
-            <ph-dots-three :size="20" />
-            <span class="content">{{ tab1.meta.arrival }}</span>
-          </div>
-        </div>
-        <!-- place -->
-        <div
-          class="explorer_modal_content_meta_place"
-          v-if="tab1.meta.place"
-        >
-          <span class="label">Lugar</span>
-          <ph-dots-three :size="20" />
-          <span class="content">{{ tab1.meta.place }}</span>
-        </div>
-        <!-- activity -->
-        <div
-          class="explorer_modal_content_meta_activity"
-          v-if="tab1.meta.activity"
-        >
-          <span class="label">Período de actividad</span>
-          <ph-dots-three :size="20" />
-          <span class="content">{{ tab1.meta.activity }}</span>
-        </div>
-      </div>
-      <!-- Goals -->
-      <div class="explorer_modal_content_collapsible" v-if="tab1.goals">
-        <collapsible :opened="false">
-          <template v-slot:handler>Objetivos</template>
-          <template v-slot:content>
-            <ul>
-              <li v-for="(item, i) in tab1.goals" :key="i" v-html="item"></li>
-            </ul>
-          </template>
-        </collapsible>
-      </div>
-      <!-- Instruments -->
-      <div
-        class="explorer_modal_content_collapsible"
-        v-if="tab1.instruments"
-      >
-        <collapsible :opened="true">
-          <template v-slot:handler>Instrumentos que transporta</template>
-          <template v-slot:content>
-            <ul>
-              <li v-for="(item, i) in tab1.instruments" :key="i" v-html="item"></li>
-            </ul>
-          </template>
-        </collapsible>
-      </div>
-      <!-- Contribution -->
-      <div
-        class="explorer_modal_content_collapsible"
-        v-if="tab1.contribution"
-      >
-        <collapsible :opened="false">
-          <template v-slot:handler>Contribución</template>
-          <template v-slot:content>
-            <ul>
-              <li v-for="(item, i) in tab1.contribution" :key="i" v-html="item"></li>
-            </ul>
-          </template>
-        </collapsible>
-      </div>
-      <!-- more info -->
-      <div v-if="tab1.moreinfo">
-        <!-- extra resources -->
-        <div class="resources">
-          <extra-resource
-            class="dark"
-            v-for="(resource, i) in tab1.assets.resources"
-            :key="i"
-            :resource="resource"
-          />
-        </div>
-      </div>
-    </div>
-
-
+    <!-- TAB 1 // machine -->
+    <machine-modal-tab-1
+      :tab1="tab1"
+      :title="machineData.title"
+      :subtitle="machineData.subtitle"
+      v-if="tab1Active"
+      @toggleTab="toggleTab"
+    />
+    <!-- TAB 2 // CAB instrument -->
+    <machine-modal-tab-2
+      :tab2="tab2"
+      :title="machineData.title"
+      :subtitle="machineData.subtitle"
+      v-if="tab2Active"
+    />
   </div>
 
 </template>
@@ -139,33 +35,21 @@
 import Vue from 'vue'
 import { mapState } from 'vuex'
 import AnimationType_04 from '../mixins/AnimationType_04'
-import { PhDotsThree } from 'phosphor-vue'
-import Collapsible from '../dsys/Collapsible.vue'
-import ExtraResource from '~/components/ExtraResource.vue'
-import {
-  CThemeProvider,
-  CTabs,
-  CTabList,
-  CTabPanels,
-  CTab,
-  CTabPanel } from '@chakra-ui/vue'
+import MachineModalTab1 from '~/components/machines/MachineModalTab1.vue'
+import MachineModalTab2 from '~/components/machines/MachineModalTab2.vue'
 
 export default Vue.extend({
   name: 'MachineModal',
   components: {
-    PhDotsThree,
-    Collapsible,
-    ExtraResource,
-    CThemeProvider,
-    CTabs,
-    CTabList,
-    CTabPanels,
-    CTab,
-    CTabPanel
+    MachineModalTab1,
+    MachineModalTab2,
   },
   data() {
     return {
       tab1: this.machineData.tabs[0],
+      tab2: this.machineData.tabs[1],
+      tab1Active: true,
+      tab2Active: false,
     }
   },
   props: ['machineData', 'open'],
@@ -173,6 +57,13 @@ export default Vue.extend({
     ...mapState({
       width: (state: any) => state.main.ui.viewPort.width,
     }),
+  },
+  methods: {
+    toggleTab(){
+      this.tab1Active = !this.tab1Active;
+      this.tab2Active = !this.tab2Active;
+    }
+
   },
   mixins: [AnimationType_04],
   async mounted() {
@@ -195,31 +86,7 @@ export default Vue.extend({
   &.opened {
     transform: translate(0);
   }
-  .btn_close{
-    @apply absolute rounded-full bg-white text-black p-2;
-    @apply ring-white ring-0 ring-opacity-50;
-    transform: translate(-50%, -50%);
-    pointer-events: initial;
-    transition: all 350ms ease;
-    background-clip: padding-box;
-    border: 3px solid white;
-    right: 0;
-  }
 
-
-  .tabPanels{
-    @apply p-4;
-    .tabPanel{
-      img{
-
-        height: 350px;
-        margin: 4em;
-      }
-      p{
-        @apply p-6 m-12;
-      }
-    }
-  }
 }
 
 /** explorer modal */
@@ -231,7 +98,7 @@ export default Vue.extend({
   align-items: center;
   position: relative;
   .cabBtn{
-    @apply flex absolute rounded-full bg-white text-black p-2;
+    @apply flex absolute rounded-full bg-white text-black px-4 py-2;
     @apply ring-white ring-0 ring-opacity-50;
     align-items: center;
     //transform: translate(-50%, -50%);
@@ -241,76 +108,24 @@ export default Vue.extend({
     border: 3px solid white;
     top: 15px;
     right: 15px;
-  }
-}
-.explorer_modal_content {
-  @apply p-6 text-gray-700;
-  width: 40%;
-  overflow-y: scroll;
-}
-.explorer_modal_content_header {
-  @apply flex justify-between items-end mb-4;
-  .explorer_modal_content_picture {
-    @apply bg-gray-200 flex-1 h-60 mr-4;
-    overflow: hidden;
-  }
-  .explorer_modal_content_agency_meta {
-    max-width: 20ch;
-    .explorer_modal_content_land {
-      @apply flex items-center;
-      .explorer_modal_content_land_picture {
-        @apply bg-gray-200 w-8 h-8 rounded-full mr-2;
-        overflow: hidden;
-        img{
-          height: 100%;
-        }
-      }
-      .explorer_modal_content_land_name {
-        @apply my-1 font-bold text-black;
-      }
+    &:hover {
+      @apply ring-white ring-4 ring-opacity-40;
+      border: 3px solid transparent;
+      transition: all 350ms ease;
+    }
+    &.active{
+      background-color: #1188fc;
+      color: white;
     }
   }
-  .explorer_modal_content_agency {
-    @apply text-sm;
+  .machine_title{
+    @apply absolute text-black;
+    top: 20px;
+    left: 20px;
+    font-size: 1.2em;
+    font-weight: bold;
   }
 }
-.explorer_modal_content_title {
-  .explorer_modal_content_title_top {
-    @apply flex justify-between items-baseline;
-    .title {
-      @apply mr-8 font-bold text-black;
-    }
-    .explorer_modal_content_success_flag {
-      @apply text-sm;
-    }
-  }
-  .explorer_modal_content_title_tagline {
-    @apply text-sm;
-  }
-}
-.explorer_modal_content_meta {
-  @apply my-4 py-4;
-  @apply border-t border-gray-400 border-b border-solid;
-  .explorer_modal_content_meta_launch,
-  .explorer_modal_content_meta_arrival,
-  .explorer_modal_content_meta_place,
-  .explorer_modal_content_meta_activity {
-    @apply flex items-center;
-    .label {
-      @apply mr-2 font-bold text-black;
-    }
-    .content {
-      @apply ml-2;
-    }
-  }
-}
-.explorer_modal_content_collapsible {
-  @apply my-4;
-}
-</style>
-<style lang="postcss">
-.collapsible_handle_label {
-  @apply font-bold text-black;
-}
+
 </style>
 
---
+++ components/machines/MachineModalTab1.vue
@@ -0,0 +1,231 @@
+<template>
+  <div class="explorer_modal_content" v-if="tab1">
+      <!-- header -->
+      <div class="explorer_modal_content_header">
+        <div class="explorer_modal_content_agency_meta">
+          <div class="explorer_modal_content_land">
+            <div class="explorer_modal_content_land_picture">
+              <img :src="tab1.picture_flag" :alt="`bandera de ${tab1.meta.land}`">
+            </div>
+            <div class="explorer_modal_content_land_name">
+              {{ tab1.meta.land }}
+            </div>
+          </div>
+          <div class="explorer_modal_content_agency">
+            {{ tab1.meta.agency }}
+          </div>
+        </div>
+      </div>
+      <!-- title -->
+      <div class="explorer_modal_content_title">
+        <div class="explorer_modal_content_title_top">
+          <h3 class="title">{{ title }}</h3>
+          <div class="explorer_modal_content_success_flag">
+            {{ tab1.meta.status }}
+          </div>
+        </div>
+        <div class="explorer_modal_content_title_tagline">
+          {{ subtitle }}
+        </div>
+      </div>
+      <!-- meta -->
+      <div class="explorer_modal_content_meta">
+        <!-- launch / arrival-->
+        <div class="explorer_modal_content_meta_launch_arrival">
+          <div
+            class="explorer_modal_content_meta_launch"
+            v-if="tab1.meta.launch"
+          >
+            <span class="label">Lanzamiento</span>
+            <ph-dots-three :size="20" />
+            <span class="content">{{ tab1.meta.launch }}</span>
+          </div>
+          <div
+            class="explorer_modal_content_meta_arrival"
+            v-if="tab1.meta.arrival"
+          >
+            <span class="label">Llegada</span>
+            <ph-dots-three :size="20" />
+            <span class="content">{{ tab1.meta.arrival }}</span>
+          </div>
+        </div>
+        <!-- place -->
+        <div
+          class="explorer_modal_content_meta_place"
+          v-if="tab1.meta.place"
+        >
+          <span class="label">Lugar</span>
+          <ph-dots-three :size="20" />
+          <span class="content">{{ tab1.meta.place }}</span>
+        </div>
+        <!-- activity -->
+        <div
+          class="explorer_modal_content_meta_activity"
+          v-if="tab1.meta.activity"
+        >
+          <span class="label">Período de actividad</span>
+          <ph-dots-three :size="20" />
+          <span class="content">{{ tab1.meta.activity }}</span>
+        </div>
+      </div>
+      <!-- Goals -->
+      <div class="explorer_modal_content_collapsible" v-if="tab1.goals">
+        <collapsible :opened="false">
+          <template v-slot:handler>Objetivos</template>
+          <template v-slot:content>
+            <ul>
+              <li v-for="(item, i) in tab1.goals" :key="i" v-html="item"></li>
+            </ul>
+          </template>
+        </collapsible>
+      </div>
+      <!-- Instruments -->
+      <div
+        class="explorer_modal_content_collapsible"
+        v-if="tab1.instruments"
+      >
+        <collapsible :opened="true">
+          <template v-slot:handler>Instrumentos que transporta</template>
+          <template v-slot:content>
+            <ul>
+              <li
+                v-for="(item, i) in tab1.instruments"
+                :key="i" v-html="item"
+                @click="item.classList.contains('toTab2') ? $emit('toggleTab') : ''">
+              </li>
+            </ul>
+          </template>
+        </collapsible>
+      </div>
+      <!-- Contribution -->
+      <div
+        class="explorer_modal_content_collapsible"
+        v-if="tab1.contribution"
+      >
+        <collapsible :opened="false">
+          <template v-slot:handler>Contribución</template>
+          <template v-slot:content>
+            <ul>
+              <li v-for="(item, i) in tab1.contribution" :key="i" v-html="item"></li>
+            </ul>
+          </template>
+        </collapsible>
+      </div>
+
+      <!-- Curiosidades -->
+      <div
+        class="explorer_modal_content_collapsible"
+        v-if="tab1.curiosities"
+      >
+        <collapsible :opened="false">
+          <template v-slot:handler>Curiosidades</template>
+          <template v-slot:content>
+            <ul>
+              <li v-for="(item, i) in tab1.curiosities" :key="i" v-html="item"></li>
+            </ul>
+          </template>
+        </collapsible>
+      </div>
+
+      <!-- more info -->
+      <div v-if="tab1.moreinfo">
+        <!-- extra resources -->
+        <div class="resources">
+          <extra-resource
+            class="dark"
+            v-for="(resource, i) in tab1.assets.resources"
+            :key="i"
+            :resource="resource"
+          />
+        </div>
+      </div>
+    </div>
+</template>
+<script>
+import { PhDotsThree } from 'phosphor-vue'
+import Collapsible from '../dsys/Collapsible.vue'
+import ExtraResource from '~/components/ExtraResource.vue'
+
+export default {
+  name: 'MachineModalTab1',
+  components: {
+    PhDotsThree,
+    Collapsible,
+    ExtraResource,
+  },
+  props: ['tab1', 'title', 'subtitle'],
+
+
+}
+</script>
+
+<style lang="postcss" scoped>
+.explorer_modal_content {
+  @apply p-6 text-gray-700;
+  width: 40%;
+  overflow-y: scroll;
+}
+.explorer_modal_content_header {
+  @apply flex justify-between items-end mb-4;
+  .explorer_modal_content_picture {
+    @apply bg-gray-200 flex-1 h-60 mr-4;
+    overflow: hidden;
+  }
+  .explorer_modal_content_agency_meta {
+    max-width: 20ch;
+    .explorer_modal_content_land {
+      @apply flex items-center;
+      .explorer_modal_content_land_picture {
+        @apply bg-gray-200 w-8 h-8 rounded-full mr-2;
+        overflow: hidden;
+        img{
+          height: 100%;
+        }
+      }
+      .explorer_modal_content_land_name {
+        @apply my-1 font-bold text-black;
+      }
+    }
+  }
+  .explorer_modal_content_agency {
+    @apply text-sm;
+  }
+}
+.explorer_modal_content_title {
+  .explorer_modal_content_title_top {
+    @apply flex justify-between items-baseline;
+    .title {
+      @apply mr-8 font-bold text-black;
+    }
+    .explorer_modal_content_success_flag {
+      @apply text-sm;
+    }
+  }
+  .explorer_modal_content_title_tagline {
+    @apply text-sm;
+  }
+}
+.explorer_modal_content_meta {
+  @apply my-4 py-4;
+  @apply border-t border-gray-400 border-b border-solid;
+  .explorer_modal_content_meta_launch,
+  .explorer_modal_content_meta_arrival,
+  .explorer_modal_content_meta_place,
+  .explorer_modal_content_meta_activity {
+    @apply flex items-center;
+    .label {
+      @apply mr-2 font-bold text-black;
+    }
+    .content {
+      @apply ml-2;
+    }
+  }
+}
+.explorer_modal_content_collapsible {
+  @apply my-4;
+  .collapsible_handle_label {
+    @apply font-bold text-black;
+  }
+}
+</style>
+
