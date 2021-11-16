fc8ae5741ea903f934cc96f36684c86ab781e9f0
caparicio-esd <caparicio@esdmadrid.es>
2021-11-16 09:54:47 +0100

    remove unused dependencies in client

--- nuxt.config.js
+++ nuxt.config.js
@@ -48,7 +48,7 @@ export default {
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
     // '@chakra-ui/nuxt',
-    '@nuxtjs/emotion'
+    // '@nuxtjs/emotion'
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
