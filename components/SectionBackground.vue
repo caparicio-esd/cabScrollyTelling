489851639266143aac21541a4e26193b4db9e32e
caparicio-esd <caparicio@esdmadrid.es>
2021-11-29 17:14:03 +0100

    add section background component for noise experim

---
+++ components/SectionBackground.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    class="section_content_background"
+    :style="{ width: `${width}px`, height: `${height}px` }"
+  >
+    <img :src="background" :alt="title" :height="height" />
+    <img class="noise" src="~/static/img/noise.gif" alt="jarl..." />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: ['width', 'height', 'title', 'background'],
+})
+</script>
+
+<style lang="postcss" scoped></style>
